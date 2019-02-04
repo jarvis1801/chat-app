d04cbde39b0b97cdce2f53bc9409d68f30dfea2c
jarvis1801 <chris26821@yahoo.com.hk>
2019-02-04 17:08:38 +0800

    add temp ui for chat list

---
+++ app/src/main/java/com/exmaple/jarvis/chat/Presenter/HomePresenter.java
@@ -0,0 +1,51 @@
+package com.exmaple.jarvis.chat.Presenter;
+
+import android.util.Log;
+
+import com.exmaple.jarvis.chat.Activity.HomeView;
+import com.exmaple.jarvis.chat.Model.Message;
+import com.exmaple.jarvis.chat.Model.User;
+import com.exmaple.jarvis.chat.Presenter.Interface.HomePresenterInterface;
+import com.exmaple.jarvis.chat.Retrofit.RetrofitFactory;
+
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class HomePresenter implements HomePresenterInterface {
+    private HomeView mView;
+    private CompositeDisposable mDisposable = new CompositeDisposable();
+
+    public HomePresenter(HomeView homeView) {
+        mView = homeView;
+    }
+
+    public void getChatMsgList(String username) {
+        Disposable disposable = RetrofitFactory.getInstance().getChatMsgList(username)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<Message>>() {
+                    @Override
+                    public void accept(@NonNull List<Message> msgList) throws Exception {
+                        mView.setAdapter(msgList);
+                        Log.e("test", "yes");
+                        Log.e("test", msgList.toString());
+                        for (int i = 0; i < msgList.size(); i++) {
+                            Log.e("test", msgList.get(i).toString());
+                        }
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(@NonNull Throwable throwable) throws Exception {
+                        throwable.printStackTrace();
+                    }
+                });
+
+        mDisposable.add(disposable);
+    }
+}
